029450d24893d44e541c26eefd9d6a1616f9013b
MCPEPIG <mcpepig123@gmail.com>
2016-07-06 18:48:25 -0700

    Im stupid.

--- src/BreakReplace/EventListener.php
+++ src/BreakReplace/EventListener.php
@@ -24,10 +24,9 @@ class EventListener implements Listener{
 		$block = $event->getBlock();
 		$item = $event->getItem();
 		$drops = $event->getDrops();
-		$vector3 = new Vector3($block->x, $block->y, $block->z);
 		if($this->plugin->getBreakReplaceStatus($player)){
 			if($item->canBePlaced()){
-				$player->getLevel()->setBlock($vector3, Block::get($item->getId(), $item->getDamage()), true, true);
+				$player->getLevel()->setBlock($block->getPosition(), Block::get($item->getId(), $item->getDamage()), true, true);
 				if(!$player->isCreative()){
 					$player->getInventory()->removeItem(Item::get($item->getId(), $item->getDamage(), 1));
 				}
