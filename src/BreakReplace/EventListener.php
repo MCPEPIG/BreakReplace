5643599991ca21535b82552b107c9c02dfa7dec1
MCPEPIG <mcpepig123@gmail.com>
2016-07-08 13:34:55 -0700

    Fixes error

--- src/BreakReplace/EventListener.php
+++ src/BreakReplace/EventListener.php
@@ -25,7 +25,7 @@ class EventListener implements Listener{
 		$item = $event->getItem();
 		$drops = $event->getDrops();
 		$vector3 = new Vector3($block->x, $block->y, $block->z);
-		if($this->plugin->getBreakReplaceStatus($player)){
+		if($this->plugin->getBRStatus($player)){
 			if($item->canBePlaced()){
 				$player->getLevel()->setBlock($vector3, Block::get($item->getId(), $item->getDamage()), true, true);
 				if(!$player->isCreative()){
