4d1f40b77f168cbaa8479ecc72b97e8eba79d13b
SOFe <sofe2038@gmail.com>
2017-03-05 19:53:32 +0800

    Removed some more redundant code

--- src/BreakReplace/EventListener.php
+++ src/BreakReplace/EventListener.php
@@ -1,11 +1,9 @@
 <?php
 namespace BreakReplace;
 
-use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
-use pocketmine\math\Vector3;
 
 class EventListener implements Listener {
     public function __construct($plugin) {
@@ -23,12 +21,11 @@ class EventListener implements Listener {
         $block = $event->getBlock();
         $item = $event->getItem();
         $drops = $event->getDrops();
-        $vector3 = new Vector3($block->x, $block->y, $block->z);
         if($this->plugin->getBRStatus($player)) {
             if($item->canBePlaced()) {
-                $player->getLevel()->setBlock($vector3, $item->getBlock(), true, true);
+                $player->getLevel()->setBlock($block, $item->getBlock(), true, true);
                 if(!$player->isCreative()) {
-                    $player->getInventory()->removeItem(Item::get($item->getId(), $item->getDamage()));
+                    $player->getInventory()->removeItem($item);
                 }
                 $player->getInventory()->addItem(...$drops);
                 $event->setCancelled();
